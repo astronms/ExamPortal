df5b3b7616a862286484ffb23b4011a4faa06433
Dawid Labuda <wazniak96@gmail.com>
2021-09-03 01:16:45 +0200

    Fixing unit-tests

--- ClientApp/src/app/guards/auth-guard.service.ts
+++ ClientApp/src/app/guards/auth-guard.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
 
   constructor(private jwtHelper: JwtHelperService, private router: Router) {
--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -6,10 +6,12 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
 
-  constructor(private http: HttpClient, private jwtHelper: JwtHelperService,) { }
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
   login(credentials) : Observable<any> {
     return this.http.post("http://localhost:5000/api/auth/login", JSON.stringify(credentials), {
