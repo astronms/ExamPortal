f5da16963f4a25dbea2f1837f33f9cdae88c06bf
astronms <astronms@gmail.com>
2021-09-10 21:20:33 +0200

    removing unnecessary elements from the demo

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { JwtModule } from "@auth0/angular-jwt";
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { ExamsComponent } from './exams/exams.component';
 
@@ -27,8 +25,6 @@ export function tokenGetter() {
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     LoginComponent,
     ExamsComponent,
     ExamComponent
@@ -39,8 +35,6 @@ export function tokenGetter() {
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'login', component: LoginComponent },
       { path: 'exams', component: ExamsComponent, canActivate: [AuthGuard]},
       { path: 'exam', component: ExamComponent, canActivate: [AuthGuard]},
