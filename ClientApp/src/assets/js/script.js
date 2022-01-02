e834ca239798c208148e68bfdedfee264193adb6
Dawid Labuda <wazniak96@gmail.com>
2022-01-02 21:28:00 +0100

    Fixing issue with error when scrolling

--- ClientApp/src/assets/js/script.js
+++ ClientApp/src/assets/js/script.js
@@ -103,12 +103,11 @@ var cardSlider = new Swiper(".card-slider", {
     },
 });
 
-/* BACK TO TOP BUTTON */
-// GET THE BUTTON
-myButton = document.getElementById("myBtn");
 
 // WHEN THE USER SCROLLS DOWN 20PX FROM THE TOP OF THE DOCUMENT, SHOW THE BUTTON
 function scrollFunctionBTT() {
+    myButton = document.getElementById("myBtn");
+
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         myButton.style.display = "block";
     } else {
