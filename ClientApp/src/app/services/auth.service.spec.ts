e8c5e3b79c54e1206e44df99b59f318a15c854a3
Dawid Labuda <wazniak96@gmail.com>
2022-01-19 19:59:55 +0100

    Completing authService UTs

--- ClientApp/src/app/services/auth.service.spec.ts
+++ ClientApp/src/app/services/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule, HttpTestingController } from "@angular/common/
 
 import { TestBed } from "@angular/core/testing";
 import { JwtModule } from "@auth0/angular-jwt";
+import { RoleEnum } from "../enums/role.enum";
 import { AuthUserModel } from "../models/auth-user.model";
 import { UserModel } from "../models/user.model";
 import { AuthService } from "./auth.service";
@@ -39,14 +40,13 @@ describe('AuthService', () => {
     });
 
     it('should return user after registration (HttpClient called once)', () => {
-        const expectedUser: UserModel = 
-            {
-                email: "blab@a.pl",
-                password: "John12343",
-                firstName: "John",
-                lastName: "Doe",
-                studentInfo: {index: 7777777}
-            }
+        const expectedUser: UserModel = {
+            email: "blab@a.pl",
+            password: "John12343",
+            firstName: "John",
+            lastName: "Doe",
+            studentInfo: {index: 7777777}
+        }
       
         authService.registerUser(expectedUser).subscribe(
             result => {
@@ -66,7 +66,7 @@ describe('AuthService', () => {
             firstName: "John",
             lastName: "Doe",
             studentInfo: {index: 7777777}
-            };
+        };
         
         
         authService.registerUser(expectedUser).subscribe(
@@ -85,6 +85,148 @@ describe('AuthService', () => {
     it('should return null if user not logged (authService.userRole)', () => {
        authService.logOut();
        expect(authService.userRole).toBeNull(); 
+    });
+
+    it('should return error if backend fails to login', () => {
+        const expectedUser = {
+            email: "blab@a.pl",
+            password: "John12343",
+        };
+        
+        
+        authService.login(expectedUser).subscribe(
+            result => 
+                fail("Should fail with 404 error"),
+            err => {
+                expect(err).toEqual('Something bad happened; please try again later.');
+            },
+            fail
+        );
+
+        const req = httpTestingController.expectOne(baseUrl + 'api/Account/login');
+        req.flush('401 Unauthorized', { status: 401, statusText: 'Unauthorized' });
+    });
+
+    it('should return true on successful login and save correct user object in LocalStorage', () => {
+        const expectedUser = {
+            email: "admin@admin.com",
+            password: "John12343",
+        };
+        
+        
+        authService.login(expectedUser).subscribe(
+            result => {
+                expect(result).toEqual(true);
+            },
+            err => {
+                fail("Shouldn't return error.");
+            }
+        );
+
+        const req = httpTestingController.expectOne(baseUrl + 'api/Account/login');
+        req.flush(
+            {
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYWRtaW5AYWRtaW4uY29tIiwiRmlyc3ROYW1lIjoiSm9obiIsIkxhc3ROYW1lIjoiRG9lIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE2NDI2MTc1MTUsImlzcyI6IkV4YW1Qb3J0YWxBUEkifQ.h1_5Yw2q5YIS01_vuQ1zhrBS8leflHlH935eeKBL5Dw"
+            }, 
+            { status: 202, statusText: 'Authorized' }
+        );
+
+        let userString = localStorage.getItem("user");
+        let user: AuthUserModel = JSON.parse(userString);
+
+        expect(userString).toBeDefined();
+        expect(user.email).toEqual("admin@admin.com");
+        expect(user.role).toEqual(RoleEnum.Admin);
+    });
+
+    it('should return false from isUserAuthenticated if token expired', () => {
+        const expectedUser = {
+            email: "admin@admin.com",
+            password: "John12343",
+        };
+        
+        
+        authService.login(expectedUser).subscribe(
+            result => {
+                expect(result).toEqual(true);
+            },
+            err => {
+                fail("Shouldn't return error.");
+            }
+        );
+
+        const req = httpTestingController.expectOne(baseUrl + 'api/Account/login');
+        req.flush(
+            {
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYWRtaW5AYWRtaW4uY29tIiwiRmlyc3ROYW1lIjoiSm9obiIsIkxhc3ROYW1lIjoiRG9lIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE2NDI2MTc1MTUsImlzcyI6IkV4YW1Qb3J0YWxBUEkifQ.h1_5Yw2q5YIS01_vuQ1zhrBS8leflHlH935eeKBL5Dw"
+            }, 
+            { status: 202, statusText: 'Authorized' }
+        );
+
+        expect(authService.isUserAuthenticated()).toBeFalsy();
+    });
+
+    it('should return null if user not logged (authService.userRole)', () => {
+        authService.logOut();
+        expect(authService.userRole).toBeNull(); 
+     });
+
+    it('should return correct user role', () => {
+        const expectedUser = {
+            email: "admin@admin.com",
+            password: "John12343",
+        };
+        
+        
+        authService.login(expectedUser).subscribe(
+            result => {
+                expect(result).toEqual(true);
+            },
+            err => {
+                fail("Shouldn't return error.");
+            }
+        );
+
+        const req = httpTestingController.expectOne(baseUrl + 'api/Account/login');
+        req.flush(
+            {
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYWRtaW5AYWRtaW4uY29tIiwiRmlyc3ROYW1lIjoiSm9obiIsIkxhc3ROYW1lIjoiRG9lIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE2NDI2MTc1MTUsImlzcyI6IkV4YW1Qb3J0YWxBUEkifQ.h1_5Yw2q5YIS01_vuQ1zhrBS8leflHlH935eeKBL5Dw"
+            }, 
+            { status: 202, statusText: 'Authorized' }
+        );
+
+        expect(authService.userRole).toEqual(RoleEnum.Admin); 
+    });
+
+    it('should logout the user', () => {
+        const expectedUser = {
+            email: "admin@admin.com",
+            password: "John12343",
+        };
+        
+        
+        authService.login(expectedUser).subscribe(
+            result => {
+                expect(result).toEqual(true);
+            },
+            err => {
+                fail("Shouldn't return error.");
+            }
+        );
+
+        const req = httpTestingController.expectOne(baseUrl + 'api/Account/login');
+        req.flush(
+            {
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYWRtaW5AYWRtaW4uY29tIiwiRmlyc3ROYW1lIjoiSm9obiIsIkxhc3ROYW1lIjoiRG9lIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE2NDI2MTc1MTUsImlzcyI6IkV4YW1Qb3J0YWxBUEkifQ.h1_5Yw2q5YIS01_vuQ1zhrBS8leflHlH935eeKBL5Dw"
+            }, 
+            { status: 202, statusText: 'Authorized' }
+        );
+
+        authService.logOut();
+
+        expect(authService.userValue).toBeNull();
+        let userString = localStorage.getItem("user");
+        expect(userString).toBeNull();
     });
 
       
