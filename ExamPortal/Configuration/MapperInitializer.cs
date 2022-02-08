07901740031701e7a66d9945211938b0ef900f55
astronms <astronms@gmail.com>
2022-02-08 22:54:58 +0100

    Added UpdateSession Endpoint

--- ExamPortal/Configuration/MapperInitializer.cs
+++ ExamPortal/Configuration/MapperInitializer.cs
@@ -45,6 +45,7 @@ namespace ExamPortal.Configuration
             CreateMap<Session, SessionDTO>().ReverseMap();
             CreateMap<Session, StudentSessionDTO>().ReverseMap();
             CreateMap<Session, CreateSessionDTO>().ReverseMap();
+            CreateMap<Session, UpdateSessionDTO>().ReverseMap();
             CreateMap<SessionDTO, CreateSessionDTO>().ReverseMap();
             //Exam
             CreateMap<Exam, ExamDTO>().ReverseMap();
