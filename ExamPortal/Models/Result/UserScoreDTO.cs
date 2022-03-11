5678e1e371c8be8dde242aba963d1e630560c6c7
astronms <astronms@gmail.com>
2022-03-11 22:34:43 +0100

    Change User Model in UserScoreDTO

--- ExamPortal/Models/Result/UserScoreDTO.cs
+++ ExamPortal/Models/Result/UserScoreDTO.cs
@@ -1,13 +1,11 @@
 ﻿using System.Security.Permissions;
+using ExamPortal.Models.Users;
 
 namespace ExamPortal.Models.Result
 {
     public class UserScoreDTO
     {
-        public string UserId { get; set; }
-        public string FristName { get; set; }
-        public string LastName { get; set; }
-        public int Index { get; set; }
+        public UserForCoursesDTO User { get; set; }
         public double Score { get; set; }
         public double MaxScore { get; set; }
     }
